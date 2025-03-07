8130d9fb3ebeccb9d0983552e9fa6eda1d2d5b86
adrien <adrien@kali>
2025-03-07 14:07:37 +0100

    init

---
+++ mvc-client/src/test/java/com/poc/mvc_client/MvcClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.mvc_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
